a7d8a3803a697c237a6d8bb4d07f357c34ba7d02
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-04-17 16:35:12 +0530

    Schema Changes

--- src/model/flat.model.js
+++ src/model/flat.model.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const flatSchema = new mongoose.Schema({
     id:{type: Number},
     block:{type: String},
-    flatNo:{type: String},
+    flatNo:{type: Number},
     totalNo:{type: Number},
+    residenttype:{type:String},
     image:{type: String}
 },
 {
